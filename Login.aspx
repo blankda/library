5088894fcd78dce11660149b2425fdfc6dc2add3
blankda <57255512+blankda@users.noreply.github.com>
2020-05-22 18:15:55 +0800

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            用户名：<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                ControlToValidate="txtName"
+                ErrorMessage="账号不能为空"></asp:RequiredFieldValidator>
+            <br />
+            密码：<asp:TextBox ID="txtPwd" runat="server"></asp:TextBox>
+            <br />
+            用户类型：<asp:RadioButtonList ID="RadioButtonList1" runat="server" DataTextField="type" DataValueField="type" RepeatDirection="Horizontal">
+                <asp:ListItem>reader</asp:ListItem>
+                <asp:ListItem>admin</asp:ListItem>
+            </asp:RadioButtonList>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="确定" OnClick="Button1_Click"/>
+        &nbsp
+        <asp:Button ID="Button2" runat="server" Text="取消" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (this.IsValid)
+        {
+            string userName = txtName.Text;    //用户名
+            string pwd = txtPwd.Text;     //密码
+            Session["entryType"] = RadioButtonList1.SelectedValue;    //记录登录用户的类型
+            string cs = Session["entryType"].ToString();
+            string sql;
+            if (Session["entryType"] == "reader")   //用户类型为读者，到读者信息表查询指定用户名和密码的记录
+            {
+                sql = "select count(*) from tb_readerInfo where readerBarCode=@name and  readerPass=@pass ";
+            }
+            else    //用户类型为管理员，到管理员信息表查询指定用户名和密码的记录
+            {
+                sql = "select count(*) from tb_user where userName=@name  and  userPwd=@pass ";
+            }
+            //调用用户名和密码验证函数
+            if (checkPwd(sql, userName, pwd))     //验证成功
+            {
+                Session["userName"] = userName;  //记录登录用户名
+                Response.Redirect("index.aspx");   //导航到系统首页
+            }
+            else    //验证失败
+            {
+                Page.RegisterStartupScript("", "<script>window.onload=function() {alert('登陆失败');}</script>");    //提示登录失败
+            }
+        }
+    }
+
+    public static bool checkPwd(string sql, string name, string pass)
+    {
+        SqlConnection con = dataOperate.createCon();         //创建数据库连接
+        con.Open();         //打开数据库连接
+        SqlCommand com = new SqlCommand(sql, con);        //创建SqlCommand对象
+        com.Parameters.Add(new SqlParameter("name", SqlDbType.VarChar, 50));        //设置参数类型
+        com.Parameters["name"].Value = name;        //设置参数值
+        com.Parameters.Add(new SqlParameter("pass", SqlDbType.VarChar, 50));       //设置参数类型
+        com.Parameters["pass"].Value = pass;        //设置参数值
+        //判断验证用户名和密码是否正确，并返回布尔值
+        if (Convert.ToInt32(com.ExecuteScalar()) > 0)   //返回指定用户名和密码的记录数大于0，此用户名和密码正确。
+        {
+            con.Close();
+            return true;
+        }
+        else
+        {
+            con.Close();
+            return false;
+        }
+    }
+
+
+}
